12712501d6818b5146d182dcda0ed6359d908ca4
André Arko <andre@arko.net>
2026-03-19 01:09:40 -0700

    try to fix the broken intel ruby-dev build

--- Formula/rv-ruby-dev.rb
+++ Formula/rv-ruby-dev.rb
@@ -8,6 +8,11 @@ class RvRubyDev < RvRuby
   def install
     system "./autogen.sh"
     super
+
+    # As of 2026-03-19, configure fails on intel with the error message
+    #   configure: error: something wrong with LDFLAGS="-Wl,-search_paths_first"
+    # So we're going to delete it, even though that worked in the last 30 daily builds.
+    ENV.delete "LDFLAGS" if Hardware::CPU.intel?
   end
 
   def stable
