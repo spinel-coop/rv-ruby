7ad42f8d439ed0cdfd6798832a7cd6b60f016d70
André Arko <andre@arko.net>
2026-07-13 21:48:18 -0700

    add ruby 4.0.6

---
+++ Formula/rv-ruby@4.0.6.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby", __dir__)
+
+class RvRubyAT406 < RvRuby
+  url "https://cache.ruby-lang.org/pub/ruby/4.0/ruby-4.0.6.tar.gz"
+  sha256 "837d299e8f7ddf2be31a229a7a7e019d354979825117989acb3b32b1a9be262a"
+end
